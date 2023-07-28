2473778c5a328ef144e5e24bdb5c370e19077c0b
Vitor Mateus <vitor7jan@gmail.com>
2023-07-27 22:29:13 -0300

    style(css): my recipe basic form

--- src/pages/my-recipes/[id].tsx
+++ src/pages/my-recipes/[id].tsx
@@ -113,109 +113,129 @@ const MyRecipePage: NextPage<MyRecipePageProps> = ({ recipe, isAdd }) => {
   return (
     <div>
       <Link href={"/my-recipes"}>my recipes</Link>
-
       <button type="button" onClick={deleteRecipe}>
         delete
       </button>
-
-      <form action="" onSubmit={saveRecipe}>
-        <label htmlFor="">
-          recipe id
-          <input
-            onChange={onChangeField}
-            type="text"
-            name="id"
-            value={currentRecipe.id}
-            disabled={!canEdit}
+      <div className="default-form">
+        <form action="" onSubmit={saveRecipe}>
+          <div className="flex flex-col md:flex-row gap-2">
+            <fieldset className="border border-orange-500 mb-1 p-3">
+              {/* <div>
+              <label htmlFor="id">recipe id</label>
+              <input
+                onChange={onChangeField}
+                type="text"
+                name="id"
+                id="id"
+                className="p-1"
+                value={currentRecipe.id}
+                disabled={!canEdit}
+              />
+            </div> */}
+              <div>
+                <label htmlFor="title">title</label>
+                <input
+                  onChange={onChangeField}
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="p-1"
+                  value={currentRecipe.title}
+                  disabled={!canEdit}
+                />
+              </div>
+              <div>
+                <label htmlFor="servings">servings</label>
+                <input
+                  onChange={onChangeField}
+                  type="number"
+                  name="servings"
+                  id="servings"
+                  className="p-1"
+                  value={currentRecipe.servings}
+                  disabled={!canEdit}
+                />
+              </div>
+              <div>
+                <label htmlFor="totalTimeInMinutes">{"total time (min)"}</label>
+                <input
+                  onChange={onChangeField}
+                  type="number"
+                  name="totalTimeInMinutes"
+                  id="totalTimeInMinutes"
+                  className="p-1"
+                  value={currentRecipe.totalTimeInMinutes}
+                  disabled={!canEdit}
+                />
+              </div>
+              {!isAdd && (
+                <>
+                  <div>
+                    <label htmlFor="updatedAt">updated at</label>
+                    <input
+                      onChange={onChangeField}
+                      type="text"
+                      name="updatedAt"
+                      id="updatedAt"
+                      className="p-1"
+                      value={currentRecipe.updatedAt}
+                      disabled={true}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="createdAt">created at</label>
+                    <input
+                      onChange={onChangeField}
+                      type="text"
+                      name="createdAt"
+                      id="createdAt"
+                      className="p-1"
+                      value={currentRecipe.createdAt}
+                      disabled={true}
+                    />
+                  </div>
+                </>
+              )}
+            </fieldset>
+            <fieldset className="border border-orange-500 mb-1 p-3">
+              <div>
+                <label htmlFor="description">description</label>
+                <textarea
+                  name="description"
+                  id="description"
+                  cols={30}
+                  rows={10}
+                  disabled={!canEdit}
+                  onChange={onChangeField}
+                  value={currentRecipe.description}
+                />
+              </div>
+            </fieldset>
+          </div>
+          <Directions
+            directions={currentRecipe.directions ?? []}
+            updateDirections={updateDirections}
+            canEdit={canEdit}
           />
-        </label>
-        <br />
-        <label htmlFor="">
-          servings
-          <input
-            onChange={onChangeField}
-            type="number"
-            name="servings"
-            value={currentRecipe.servings}
-            disabled={!canEdit}
+          <Ingredients
+            ingredients={currentRecipe.ingredients ?? []}
+            updateIngredients={updateIngredients}
+            canEdit={canEdit}
           />
-        </label>
-        <br />
-        <label htmlFor="">
-          title
-          <input
-            onChange={onChangeField}
-            type="text"
-            name="title"
-            value={currentRecipe.title}
-            disabled={!canEdit}
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          totalTimeInMinutes
-          <input
-            onChange={onChangeField}
-            type="number"
-            name="totalTimeInMinutes"
-            value={currentRecipe.totalTimeInMinutes}
-            disabled={!canEdit}
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          updatedAt
-          <input
-            onChange={onChangeField}
-            type="text"
-            name="updatedAt"
-            value={currentRecipe.updatedAt}
-            disabled={!canEdit}
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          createdAt
-          <input
-            onChange={onChangeField}
-            type="text"
-            name="createdAt"
-            value={currentRecipe.createdAt}
-            disabled={!canEdit}
-          />
-        </label>
-        <br />
-        <textarea
-          name="description"
-          cols={30}
-          rows={10}
-          disabled={!canEdit}
-          onChange={onChangeField}
-          value={currentRecipe.description}
-        />
-        <Directions
-          directions={currentRecipe.directions ?? []}
-          updateDirections={updateDirections}
-          canEdit={canEdit}
-        />
-        <Ingredients
-          ingredients={currentRecipe.ingredients ?? []}
-          updateIngredients={updateIngredients}
-          canEdit={canEdit}
-        />
-        {canEdit ? (
-          <>
-            <button type="button" onClick={cancelEdit}>
-              Cancel
+          {canEdit ? (
+            <>
+              <button type="button" onClick={cancelEdit}>
+                Cancel
+              </button>
+              <button type="submit">Save</button>
+            </>
+          ) : (
+            <button type="button" onClick={() => setCanEdit(true)}>
+              Edit
             </button>
-            <button type="submit">Save</button>
-          </>
-        ) : (
-          <button type="button" onClick={() => setCanEdit(true)}>
-            Edit
-          </button>
-        )}
-      </form>
+          )}
+        </form>
+      </div>
       <Comments recipeId={currentRecipe.id ?? ""} />
     </div>
   );
