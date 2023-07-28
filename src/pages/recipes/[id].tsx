9099bc0cece48542ec2a8e43e9974fc322b6e31b
Vitor Mateus <vitor7jan@gmail.com>
2023-07-28 00:25:03 -0300

    style(css): recipe detail

--- src/pages/recipes/[id].tsx
+++ src/pages/recipes/[id].tsx
@@ -25,36 +25,74 @@ const RecipePage: NextPage<RecipePageProps> = ({ recipe }) => {
     user,
   } = recipe;
 
+  let directionsCounter = 1;
+  let ingredientesCounter = 1;
+
   return (
-    <div>
-      <Link href={"/recipes"}>home</Link>
-      <br />
-      {`${id} ${servings} ${title} ${totalTimeInMinutes} ${updatedAt}`}
-      <br />
-      {`${createdAt} ${description} `}
-      <br />
-      <span>{`${user?.id} - ${user?.name}`}</span>
-      <br />
-      <span>directions</span>
-      <ul>
-        {directions?.map(({ description }) => {
-          return <li key={Math.random() * 99}>{description}</li>;
-        })}
-      </ul>
-      <br />
-      <span>ingredients</span>
-      <br />
-      <ul>
-        {ingredients?.map(({ product, quantity, unitOfMeasurement }) => {
-          return (
-            <li
-              key={Math.random() * 99}
-            >{`${product} - ${quantity} - ${unitOfMeasurement}`}</li>
-          );
-        })}
-      </ul>
-      <br />
-      <Comments recipeId={id ?? ""} />
+    <div className="w-screen">
+      <div className="mx-auto w-10/12 pt-4">
+        <h1 className="text-center text-orange-600 text-3xl">{title}</h1>
+        <div className="flex justify-around">
+          <div>
+            <strong>servings:</strong>
+            <span className="ml-2">{servings}</span>
+          </div>
+          <div>
+            <strong>done in:</strong>
+            <span className="ml-2">{`${totalTimeInMinutes} min`}</span>
+          </div>
+        </div>
+        <div className="text-center">
+          <strong>posted by:</strong>
+          <span className="ml-2">{user?.name}</span>
+        </div>
+        <div className="flex justify-around">
+          <div>
+            <strong>updated at:</strong>
+            <span className="ml-2">{updatedAt}</span>
+          </div>
+          <div>
+            <strong>created at:</strong>
+            <span className="ml-2">{createdAt}</span>
+          </div>
+        </div>
+        <div className="p-2">
+          <p>{description}</p>
+        </div>
+        <div className="flex flex-col justify-center items-center my-4">
+          <h4 className="text-center text-orange-600 text-2xl">directions</h4>
+          <ul>
+            {directions?.map(({ description }) => {
+              const currentDirection = directionsCounter++;
+              return (
+                <li key={currentDirection}>
+                  <strong className="text-orange-600">{`${currentDirection} - `}</strong>
+                  <span>{description}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="flex flex-col justify-center items-center my-4">
+          <h4 className="text-center text-orange-600 text-2xl">ingredients</h4>
+          <ul>
+            {ingredients?.map(({ product, quantity, unitOfMeasurement }) => {
+              const currentIngredient = ingredientesCounter++;
+              return (
+                <li key={currentIngredient}>
+                  <strong className="text-orange-600">{`${currentIngredient} - `}</strong>
+                  <div className="inline-flex gap-2">
+                    <span>{quantity}</span>
+                    <span>{unitOfMeasurement}</span>
+                    <span>{product}</span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <Comments recipeId={id ?? ""} />
+      </div>
     </div>
   );
 };
