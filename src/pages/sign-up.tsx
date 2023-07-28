b16370c92f8a1c9705abce5beaa6a7bd429c619e
Vitor Mateus <vitor7jan@gmail.com>
2023-07-27 21:08:57 -0300

    style(css): exported commom styles to global.css

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -38,45 +38,39 @@ const SignUpPage: NextPage = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center p-2">
-      <h1 className="text-3xl text-center mb-3 text-orange-600">register</h1>
-      <form action="" onSubmit={signUp} className="flex flex-col items-center">
-        <fieldset className="flex flex-col justify-evenly items-center border border-orange-500 p-3 mb-1 rounded-md">
-          <div className="grid grid-cols-6 my-1 items-center">
-            <label htmlFor="name" className="col-span-2 text-right pr-3">
-              name
-            </label>
+    <div className="default-form">
+      <h1 className="text-3xl mb-3">register</h1>
+      <form action="" onSubmit={signUp}>
+        <fieldset className="border border-orange-500 mb-1 p-3">
+          <div>
+            <label htmlFor="name">name</label>
             <input
               type="text"
               name="name"
               id="name"
-              className="col-span-4 rounded-md hover:bg-orange-100 p-1"
+              className="p-1"
               onChange={onChangeField}
               value={currentUser.name}
             />
           </div>
-          <div className="grid grid-cols-6 my-1 items-center">
-            <label htmlFor="email" className="col-span-2 text-right pr-3">
-              email
-            </label>
+          <div>
+            <label htmlFor="email">email</label>
             <input
               type="email"
               name="email"
               id="email"
-              className="col-span-4 rounded-md hover:bg-orange-100 p-1"
+              className="p-1"
               onChange={onChangeField}
               value={currentUser.email}
             />
           </div>
-          <div className="grid grid-cols-6 my-1 items-center">
-            <label htmlFor="password" className="col-span-2 text-right pr-3">
-              password
-            </label>
+          <div>
+            <label htmlFor="password">password</label>
             <input
               type="password"
               name="password"
               id="password"
-              className="col-span-4 rounded-md hover:bg-orange-100 p-1"
+              className="p-1"
               onChange={onChangeField}
               value={currentUser.password}
             />
@@ -84,7 +78,7 @@ const SignUpPage: NextPage = () => {
         </fieldset>
         <button
           type="submit"
-          className="w-full bg-orange-500 text-gray-100 rounded-md p-1 text-xl hover:text-orange-500 hover:bg-orange-200 transition-all"
+          className="bg-orange-500 p-1 text-xl hover:text-orange-500 hover:bg-orange-200 text-gray-100"
         >
           register
         </button>
