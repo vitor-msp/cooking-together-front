d467db68d15d7beee48709832ab97d8bf9a08d62
Vitor Mateus <vitor7jan@gmail.com>
2023-07-27 22:14:10 -0300

    style(css): form search recipes

--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -55,88 +55,108 @@ const RecipesPage: NextPage<RecipesPageProps> = ({ recipes }) => {
   };
 
   return (
-    <div>
-      <h1>RecipesPage</h1>
-
-      <form action="" onSubmit={onSubmit}>
-        <br />
-        <label htmlFor="">
-          title
-          <input
-            type="text"
-            name="title"
-            value={currentSearch.title ?? ""}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          servingsFrom
-          <input
-            type="number"
-            name="servingsFrom"
-            value={currentSearch.servingsFrom ?? ""}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          servingsTo
-          <input
-            type="number"
-            name="servingsTo"
-            value={currentSearch.servingsTo ?? ""}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          totalTimeInMinutesFrom
-          <input
-            type="number"
-            name="totalTimeInMinutesFrom"
-            value={currentSearch.totalTimeInMinutesFrom ?? ""}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          totalTimeInMinutesTo
-          <input
-            type="number"
-            name="totalTimeInMinutesTo"
-            value={currentSearch.totalTimeInMinutesTo ?? ""}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <label htmlFor="">
-          ingredients
-          <input
-            type="text"
-            name="ingredients"
-            value={currentSearch.ingredients ?? ""}
-            onChange={onChangeField}
-          />
-        </label>
-        <br />
-        <button type="submit">search</button>
-      </form>
-
-      <ul>
-        {recipes?.map(
-          ({ id, servings, title, totalTimeInMinutes, updatedAt }) => {
-            return (
-              <li key={id}>
-                {/* passHref */}
-                <Link href={`/recipes/${id}`}>
-                  {`${servings} ${title} ${totalTimeInMinutes} ${updatedAt}`}
-                </Link>
-              </li>
-            );
-          }
-        )}
-      </ul>
+    <div className="flex flex-col items-center justify-start self-start w-screen">
+      <div className="default-form">
+        <form action="" onSubmit={onSubmit}>
+          <div className="flex flex-col md:flex-row gap-2">
+            <fieldset className="border border-orange-500 mb-1 p-3">
+              <div>
+                <label htmlFor="title">title contains</label>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="p-1"
+                  value={currentSearch.title ?? ""}
+                  onChange={onChangeField}
+                />
+              </div>
+              <div>
+                <label htmlFor="servingsFrom">servings from</label>
+                <input
+                  type="number"
+                  name="servingsFrom"
+                  id="servingsFrom"
+                  className="p-1"
+                  value={currentSearch.servingsFrom ?? ""}
+                  onChange={onChangeField}
+                />
+              </div>
+              <div>
+                <label htmlFor="servingsTo">servings to</label>
+                <input
+                  type="number"
+                  name="servingsTo"
+                  id="servingsTo"
+                  className="p-1"
+                  value={currentSearch.servingsTo ?? ""}
+                  onChange={onChangeField}
+                />
+              </div>
+            </fieldset>
+            <fieldset className="border border-orange-500 mb-1 p-3">
+              <div>
+                <label htmlFor="totalTimeInMinutesFrom">
+                  {"total time from (min)"}
+                </label>
+                <input
+                  type="number"
+                  name="totalTimeInMinutesFrom"
+                  id="totalTimeInMinutesFrom"
+                  className="p-1"
+                  value={currentSearch.totalTimeInMinutesFrom ?? ""}
+                  onChange={onChangeField}
+                />
+              </div>
+              <div>
+                <label htmlFor="totalTimeInMinutesTo">
+                  {" total time to (min)"}
+                </label>
+                <input
+                  type="number"
+                  name="totalTimeInMinutesTo"
+                  id="totalTimeInMinutesTo"
+                  className="p-1"
+                  value={currentSearch.totalTimeInMinutesTo ?? ""}
+                  onChange={onChangeField}
+                />
+              </div>
+              <div>
+                <label htmlFor="ingredients">ingredients contains</label>
+                <input
+                  type="text"
+                  name="ingredients"
+                  id="ingredients"
+                  className="p-1"
+                  value={currentSearch.ingredients ?? ""}
+                  onChange={onChangeField}
+                />
+              </div>
+            </fieldset>
+          </div>
+          <button
+            type="submit"
+            className="bg-orange-500 p-1 text-xl hover:text-orange-500 hover:bg-orange-200 text-gray-100"
+          >
+            search for recipes
+          </button>
+        </form>
+      </div>
+      <div className="">
+        <ul>
+          {recipes?.map(
+            ({ id, servings, title, totalTimeInMinutes, updatedAt }) => {
+              return (
+                <li key={id}>
+                  <Link href={`/recipes/${id}`}>
+                    {`${servings} ${title} ${totalTimeInMinutes} ${updatedAt}`}
+                  </Link>
+                </li>
+              );
+            }
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
